b78a1369f8b8045efe1778fe707797391399f0a3
Ruslan Bessarab <rbessarab@mail.greenriver.edu>
2021-01-28 15:15:18 -0500

    added form 1, form 2 and summary

--- index.php
+++ index.php
@@ -18,15 +18,18 @@ $f3->route('GET /', function () {
 });
 
 $f3->route('GET /order', function () {
-    echo "order";
+    $view = new Template();
+    echo $view->render('views/form1.html');
 });
 
 $f3->route('GET /order2', function () {
-    echo "order 2";
+    $view = new Template();
+    echo $view->render('views/form2.html');
 });
 
 $f3->route('GET /summary', function () {
-    echo "summary";
+    $view = new Template();
+    echo $view->render('views/summary.html');
 });
 
 //run fat free (need to be the last)
